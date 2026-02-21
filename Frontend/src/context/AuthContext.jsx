29824642246f525ef37f05b1d8e9022eb896606c
Atul <atulyadav06390639@gmail.com>
2026-02-22 02:00:59 +0530

    Frontend

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/axios";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      api.get("/auth/profile")
+        .then(res => setUser(res.data))
+        .catch(() => logout());
+    }
+  }, []);
+
+  const login = async (data) => {
+    const res = await api.post("/auth/login", data);
+    localStorage.setItem("token", res.data.token);
+    setUser(res.data.user);
+
+    if (res.data.user.role === "admin")
+      navigate("/admin/dashboard");
+    else
+      navigate("/user/dashboard");
+  };
+
+  const register = async (data) => {
+    const res = await api.post("/auth/register", data);
+    localStorage.setItem("token", res.data.token);
+    setUser(res.data.user);
+    navigate("/user/dashboard");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/login");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
